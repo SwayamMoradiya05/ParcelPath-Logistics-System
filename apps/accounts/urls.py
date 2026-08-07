0ca5303a30d0a76e627de843d10d3bd8fcf34170
Swayam <krishsanagani13@gmail.com>
2026-08-07 11:44:48 +0530

    forgot passowrd

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
@@ -5,34 +6,81 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
+
     path(
         "login/",
         views.login_view,
         name="login",
     ),
+
     path(
         "logout/",
         views.logout_view,
         name="logout",
     ),
+
     path(
         "register/",
         views.register_view,
         name="register",
     ),
+
     path(
         "profile/",
         views.profile_view,
         name="profile",
     ),
+
     path(
         "profile/edit/",
         views.edit_profile_view,
         name="edit_profile",
     ),
+
     path(
         "change-password/",
         views.change_password_view,
         name="change_password",
     ),
+
+    # ======================================================
+    # Forgot Password
+    # ======================================================
+
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="accounts/forgot_password.html",
+            email_template_name="accounts/password_reset_email.html",
+            subject_template_name="accounts/password_reset_subject.txt",
+            success_url="/accounts/password-reset/done/",
+        ),
+        name="password_reset",
+    ),
+
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="accounts/password_reset_done.html",
+        ),
+        name="password_reset_done",
+    ),
+
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="accounts/password_reset_confirm.html",
+            success_url="/accounts/reset/done/",
+        ),
+        name="password_reset_confirm",
+    ),
+
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="accounts/password_reset_complete.html",
+        ),
+        name="password_reset_complete",
+    ),
+
 ]
--- config/settings.py
+++ config/settings.py
@@ -214,10 +214,19 @@ AUTHENTICATION_BACKENDS = [
 # Email
 # -----------------------------------------------------------------------------
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-DEFAULT_FROM_EMAIL = "ParcelPath <no-reply@parcelpath.com>"
+EMAIL_HOST = "smtp.gmail.com"
 
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = "moradiyaswayam@gmail.com"
+
+EMAIL_HOST_PASSWORD = "wwyc mtup pnvu mtgo"
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # -----------------------------------------------------------------------------
 # Session Settings
